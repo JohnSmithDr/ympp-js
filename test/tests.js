39dd43a6c34cf91bf2895fd19cab1d98130c424d
johnsmith1st <johnsmith1st@live.com>
2016-10-31 17:32:21 +0800

    add ympp

--- test/tests.js
+++ test/tests.js
@@ -2,8 +2,13 @@
 
 describe('tests', function () {
 
-  require('./impp-core/tests');
-  require('./lib/ympp-message');
-  require('./examples/tests');
+  describe('ympp-core', function () {
+    require('./lib/test-utils');
+    require('./lib/test-ympp-message');
+  });
+
+  describe('examples', function () {
+
+  });
 
 });
