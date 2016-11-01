52d4f332e639d27cc68aa22bd6d0d7dd876acb29
johnsmith1st <johnsmith1st@live.com>
2016-11-01 14:19:51 +0800

    refine protocol

--- test/lib/test-ympp-message.js
+++ test/lib/test-ympp-message.js
@@ -2,54 +2,116 @@
 
 const expect = require('chai').expect;
 const ympp = require('../../lib/');
-const logger = require('../logger');
 
 describe('ympp-message', function () {
 
-  it('should be ok', function () {
+  describe('.message()', function () {
 
-    let time = +Date.now();
+    it('should be ok', function () {
 
-    let msg = ympp.message({
-      intent: 'greeting',
-      header: {
-        id: '1',
-        from: ['alice'],
-        to: ['bob', 'carter'],
-        channel: '101',
-        time: time,
-        extra: {
-          foo: 'FOO',
-          bar: 'BAR'
+      let time = +Date.now();
+
+      let msg = ympp.message({
+        intent: 'greeting',
+        header: {
+          id: '1',
+          to: ['bob', 'carter'],
+          from: 'alice',
+          channel: '101',
+          time: time,
+          extra: {
+            foo: 'FOO',
+            bar: 'BAR'
+          }
+        },
+        content: {
+          encoding: 'plain',
+          charset: 'utf8',
+          type: 'text/plain',
+          data: new Buffer('Hello !!!', 'utf8')
         }
-      },
-      content: {
-        encoding: 'plain',
-        charset: 'utf8',
-        type: 'text/plain',
-        data: new Buffer('Hello World', 'utf8')
-      }
+      });
+
+      let encoded = ympp.message.encode(msg);
+      let decoded = ympp.message.decode(encoded);
+
+      expect(decoded.intent).to.equal('greeting');
+      expect(decoded.header.id).to.equal('1');
+      expect(decoded.header.to).to.include.members(['bob', 'carter']);
+      expect(decoded.header.from).to.equal('alice');
+      expect(decoded.header.channel).to.equal('101');
+      expect(decoded.header.time.toNumber()).to.equal(time);
+      expect(decoded.header.extra.get('foo')).to.equal('FOO');
+      expect(decoded.header.extra.get('bar')).to.equal('BAR');
+      expect(decoded.content.encoding).to.equal('plain');
+      expect(decoded.content.charset).to.equal('utf8');
+      expect(decoded.content.type).to.equal('text/plain');
+      expect(decoded.content.data.toBuffer().toString()).to.equal('Hello !!!');
+
     });
 
-    let encoded = ympp.message.encode(msg);
-    let decoded = ympp.message.decode(encoded);
+  });
 
-    logger.debug(encoded.length);
-    logger.debug(encoded);
-    logger.debug(decoded);
+  describe('.header()', function () {
 
-    expect(decoded.intent).to.equal('greeting');
-    expect(decoded.header.id).to.equal('1');
-    expect(decoded.header.from).to.include('alice');
-    expect(decoded.header.to).to.include.members(['bob', 'carter']);
-    expect(decoded.header.channel).to.equal('101');
-    expect(decoded.header.time.toNumber()).to.equal(time);
-    expect(decoded.header.extra.get('foo')).to.equal('FOO');
-    expect(decoded.header.extra.get('bar')).to.equal('BAR');
-    expect(decoded.content.encoding).to.equal('plain');
-    expect(decoded.content.charset).to.equal('utf8');
-    expect(decoded.content.type).to.equal('text/plain');
-    expect(decoded.content.data.toBuffer().toString()).to.equal('Hello World');
+    it('should be ok', function () {
+
+      let time = +Date.now();
+
+      let msg = ympp.message({
+        intent: 'present'
+      });
+
+      msg.header = ympp.message.header({
+        to: ['bob', 'carter'],
+        from: 'alice',
+        time: time
+      });
+
+      let encoded = ympp.message.encode(msg);
+      let decoded = ympp.message.decode(encoded);
+
+      expect(decoded.header.to).to.include.members(['bob', 'carter']);
+      expect(decoded.header.from).to.equal('alice');
+      expect(decoded.header.time.toNumber()).to.equal(time);
+
+    });
+
+  });
+
+  describe('.content()', function () {
+
+    it('should be ok', function () {
+
+      let d = { message: 'Hello !!!' };
+      let time = +Date.now();
+
+      let msg = ympp.message({
+        intent: 'im',
+        header: {
+          to: ['bob'],
+          from: 'alice',
+          time: time
+        }
+      });
+
+      msg.content = ympp.message.content({
+        encoding: 'plain',
+        charset: 'utf8',
+        type: 'application/json',
+        data: new Buffer(JSON.stringify(d), 'utf8')
+      });
+
+      let encoded = ympp.message.encode(msg);
+      let decoded = ympp.message.decode(encoded);
+
+      expect(decoded.content.encoding).to.equal('plain');
+      expect(decoded.content.charset).to.equal('utf8');
+      expect(decoded.content.type).to.equal('application/json');
+      expect(JSON.parse(decoded.content.data.toBuffer().toString())).to.deep.equal(d);
+
+    });
+
   });
 
 });
